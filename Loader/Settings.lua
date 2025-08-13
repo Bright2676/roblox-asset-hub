85da3f1e31b0affefaf2c289f5a520911a6984a6
Bright <buttons821@gmail.com>
2025-08-13 19:09:45 -0400

    Create Settings.lua

---
+++ Loader/Settings.lua
@@ -0,0 +1,19 @@
+--[[
+
+	» Roblox Asset Hub | Loader Settings | «
+	
+	General Loader Settings v0.1.0
+	
+	Written by iiBright2676
+	**DO NOT TOUCH UNLESS YOU ARE EDITING SETTINGS OR FORKING.**
+
+]]--
+
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+return { -- you CAN touch this stuff
+	Settings = {
+		HubName = "GitHub Testing",
+		VerboseDebugs = false,
+	},
+}
